bb8972902b873232358bf364a79f26b143d7d42a
joeroser <joeroser@gmail.com>
2026-02-27 11:08:25 +0000

    added testing for Logger

--- src/monitoring.zig
+++ src/monitoring.zig
@@ -1,6 +1,7 @@
 //! Tools for monitoring your code
 
 const std = @import("std");
+const testing = std.testing;
 const Writer = std.Io.Writer;
 
 /// A logger that will write to any writer interface
@@ -14,32 +15,45 @@ pub const Logger = struct {
         };
     }
 
-    pub fn plain(self: *Logger, comptime format: []const u8, args: anytype) !void {
+    pub fn plain(self: *Logger, comptime format: []const u8, args: anytype) Error!void {
         try self.writer.print(format ++ "\n", args);
     }
-    pub fn debug(self: *Logger, comptime format: []const u8, args: anytype) !void {
+    pub fn debug(self: *Logger, comptime format: []const u8, args: anytype) Error!void {
         try self.writer.print("[Debug] " ++ format ++ "\n", args);
     }
-    pub fn info(self: *Logger, comptime format: []const u8, args: anytype) !void {
+    pub fn info(self: *Logger, comptime format: []const u8, args: anytype) Error!void {
         try self.writer.print("[Info] " ++ format ++ "\n", args);
     }
-    pub fn warn(self: *Logger, comptime format: []const u8, args: anytype) !void {
+    pub fn warn(self: *Logger, comptime format: []const u8, args: anytype) Error!void {
         try self.writer.print("[Warn] " ++ format ++ "\n", args);
     }
-    pub fn err(self: *Logger, comptime format: []const u8, args: anytype) !void {
+    pub fn err(self: *Logger, comptime format: []const u8, args: anytype) Error!void {
         try self.writer.print("[Err] " ++ format ++ "\n", args);
     }
     pub fn flush(self: *Logger) !void {
         try self.writer.flush();
     }
+
+    const Error = std.Io.Writer.Error;
 };
 
-test "infoLog" {
+test "Types" {
+    const msg = "Testing Logger";
+    const cases = .{
+        .{ .func = Logger.plain, .expected = msg ++ "\n" },
+        .{ .func = Logger.info, .expected = "[Info] " ++ msg ++ "\n" },
+        .{ .func = Logger.debug, .expected = "[Debug] " ++ msg ++ "\n" },
+        .{ .func = Logger.warn, .expected = "[Warn] " ++ msg ++ "\n" },
+        .{ .func = Logger.err, .expected = "[Err] " ++ msg ++ "\n" },
+    };
+
     var buf: [1024]u8 = undefined;
     var mw: Writer = .fixed(&buf);
-    var log = Logger.init(&mw);
+    var log: Logger = .init(&mw);
 
-    try log.info("A logging test", .{});
-
-    try std.testing.expectEqualStrings("[Info] A logging test\n", mw.buffer[0..mw.end]);
+    inline for (cases) |case| {
+        try case.func(&log, msg, .{});
+        try testing.expectEqualStrings(case.expected, mw.buffer[0..mw.end]);
+        mw.end = 0;
+    }
 }
--- src/root.zig
+++ src/root.zig
@@ -1,2 +1,7 @@
 const std = @import("std");
 const monitoring = @import("monitoring");
+const testing = @import("testing.zig");
+
+test "recurse" {
+    std.testing.refAllDecls(@This());
+}
