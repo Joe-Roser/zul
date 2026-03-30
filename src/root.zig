113e70a69aafdd3d88fdfb588c0084d44a0b656a
joeroser <joeroser@gmail.com>
2026-03-30 19:03:51 +0100

    Added logger to pub

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
-const monitoring = @import("monitoring");
-const testing = @import("testing.zig");
+pub const monitoring = @import("monitoring");
+pub const testing = @import("testing.zig");
 
 test "recurse" {
     std.testing.refAllDecls(@This());
